a4de23ea1fe277e04ada31c03c5a9caa9926a651
HyeJi Jeong <102470319+hyejizip@users.noreply.github.com>
2023-05-01 01:16:43 +0900

    알림장 작성 UI - 카메라/갤러리 수정

--- lib/Setup/SetupPage.dart
+++ lib/Setup/SetupPage.dart
@@ -41,6 +41,7 @@ class _SetupPageState extends State<SetupPage> {
         onTap: () { pageAnimation(context, AddFacilities()); });
   }
 
+
   Widget appProfile() {
     return ListTile(
         title: Text('내 정보'),
