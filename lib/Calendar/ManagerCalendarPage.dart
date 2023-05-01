2ef597679eebaee63a713a586111dffe50356e11
HyeJi Jeong <102470319+hyejizip@users.noreply.github.com>
2023-05-02 01:09:50 +0900

    시설 추가하기 화면 - 전화번호 입력 형식 수정

--- lib/Calendar/ManagerCalendarPage.dart
+++ lib/Calendar/ManagerCalendarPage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import '/Supplementary/ThemeColor.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 ThemeColor themeColor = ThemeColor();
 
